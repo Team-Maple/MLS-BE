21ebdc4a92078ad73938918d3f8ccd5b53adb2f9
mungmnb777 <mungmnb777@gmail.com>
2025-05-14 21:14:10 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/maple/api/MapleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maple.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
