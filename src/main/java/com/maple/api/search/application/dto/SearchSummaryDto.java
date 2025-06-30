b462119ba61bdb677b2f14e10964b499bf567b6d
mungmnb777 <mungmnb777@gmail.com>
2025-07-01 00:59:09 +0900

    fix: 잘못된 DTO 필드 수정

--- src/main/java/com/maple/api/search/application/dto/SearchSummaryDto.java
+++ src/main/java/com/maple/api/search/application/dto/SearchSummaryDto.java
@@ -2,7 +2,7 @@ package com.maple.api.search.application.dto;
 
 import com.maple.api.search.domain.VwSearchSummary;
 
-public record SearchSummaryDto(Integer originalId, String name, String imageUrl, String category) {
+public record SearchSummaryDto(Integer originalId, String name, String imageUrl, String type) {
     public static SearchSummaryDto toDto(VwSearchSummary entity) {
         return new SearchSummaryDto(entity.getOriginalId(), entity.getName(), entity.getImageUrl(), entity.getType());
     }
