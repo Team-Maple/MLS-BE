93d59736151f6a9cc74c97181f28ae5a6af1dde4
mungmnb777 <mungmnb777@gmail.com>
2025-07-06 20:07:58 +0900

    feat: 쿼리 파라미터에 더 적절한 @ParameterObject로 변경

--- src/main/java/com/maple/api/item/presentation/restapi/ItemController.java
+++ src/main/java/com/maple/api/item/presentation/restapi/ItemController.java
@@ -11,7 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +24,7 @@ public class ItemController {
 
     @GetMapping
     public ResponseEntity<ResponseTemplate<Page<ItemSummaryDto>>> searchItems(
-            @Valid @ModelAttribute ItemSearchRequestDto searchRequest,
+            @Valid @ParameterObject ItemSearchRequestDto searchRequest,
             @PageableDefault(size = 20, sort = "name") Pageable pageable) {
         
         Page<ItemSummaryDto> results = itemService.searchItems(searchRequest, pageable);
