b47e7712c40dcaadbfcb384c7440bcf06d9ff2f6
mungmnb777 <mungmnb777@gmail.com>
2025-08-12 22:10:08 +0900

    feat: DTO Validation NotEmpty만 두기

--- src/main/java/com/maple/api/bookmark/application/dto/CollectionAddBookmarksRequestDto.java
+++ src/main/java/com/maple/api/bookmark/application/dto/CollectionAddBookmarksRequestDto.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 @Schema(description = "컬렉션에 북마크 추가 요청")
 public record CollectionAddBookmarksRequestDto(
-        @NotNull(message = "북마크 ID 리스트는 필수입니다.")
         @NotEmpty(message = "최소 1개 이상의 북마크 ID가 필요합니다.")
         @UniqueElements(message = "중복된 북마크 ID가 있습니다.")
         @Schema(description = "추가할 북마크 ID 리스트", example = "[1, 2, 3]")
