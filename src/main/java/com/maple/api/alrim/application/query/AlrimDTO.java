cb6e133c79d092cef6e4af6439770536bb9f8f9d
jaehyeok jin <jjh2613@gmail.com>
2025-12-12 20:05:13 +0900

    bugfix: json 포맷 수정

--- src/main/java/com/maple/api/alrim/application/query/AlrimDTO.java
+++ src/main/java/com/maple/api/alrim/application/query/AlrimDTO.java
@@ -1,8 +1,8 @@
 package com.maple.api.alrim.application.query;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.maple.api.alrim.domain.Alrim;
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -15,7 +15,7 @@ public record AlrimDTO(
   @Schema(description = "알림정보 LINK. 알림의 ID 로 사용하며, 알림을 읽었는지를 판단하는 기준이 됩니다.")
   String link,
   @Schema(description = "등록일자")
-  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
   LocalDateTime date
 ) {
   public static AlrimDTO toDTO(Alrim alrim) {
