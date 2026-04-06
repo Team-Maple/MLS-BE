bbe49fe51a91a477d2b72c76c26f6dda61ac187b
mungmnb777 <mungmnb777@gmail.com>
2026-04-06 21:58:21 +0900

    feat: AlrimRead에 alrimId 추가

--- src/main/java/com/maple/api/alrim/domain/AlrimRead.java
+++ src/main/java/com/maple/api/alrim/domain/AlrimRead.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "alrim_read", indexes = {
   @Index(name = "idx_alrim_read_member_id_alrim_link", columnList = "memberId, alrimLink"),
+  @Index(name = "idx_alrim_read_member_id_alrim_id", columnList = "memberId, alrimId"),
 })
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -23,10 +24,18 @@ public class AlrimRead {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  private Long alrimId;
   private String alrimLink;
   private String memberId;
 
   public AlrimRead(String alrimLink, String memberId) {
+    this.alrimId = null;
+    this.alrimLink = alrimLink;
+    this.memberId = memberId;
+  }
+
+  public AlrimRead(Long alrimId, String alrimLink, String memberId) {
+    this.alrimId = alrimId;
     this.alrimLink = alrimLink;
     this.memberId = memberId;
   }
