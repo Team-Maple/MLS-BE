b96d720f3e15c6888ca0990466db6fc2e27df61f
mungmnb777 <mungmnb777@gmail.com>
2026-04-07 00:28:08 +0900

    fix: 기존에 테스트용으로 1분마다 크롤링하던거 원복

--- src/main/java/com/maple/api/alrim/application/command/AlrimEventBatch.java
+++ src/main/java/com/maple/api/alrim/application/command/AlrimEventBatch.java
@@ -23,8 +23,7 @@ public class AlrimEventBatch {
 
   // 서버 다중화시 락 필요
   // 매일 09:00~20:00 사이 0분에 실행
-  @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
-//  @Scheduled(cron = "0 0 9-20 * * *", zone = "Asia/Seoul")
+  @Scheduled(cron = "0 0 9-20 * * *", zone = "Asia/Seoul")
 //  @Scheduled(initialDelay = 0, fixedRate = 60 * 1000 * 60) // 1시간마다 실행 (테스트용)
   public void runCollectBatch() {
     val fcmCandidates = new ArrayList<Alrim>();
@@ -34,7 +33,7 @@ public class AlrimEventBatch {
       val notices = crawler.crawlNotices();
       val noticesWithoutExisting = mapleLandAlrimBatchService.excludeExistingFromCrawledAlrims(AlrimType.NOTICE, notices);
       commandBatchMediator.saveAll(noticesWithoutExisting);
-//      fcmCandidates.addAll(noticesWithoutExisting);
+      fcmCandidates.addAll(noticesWithoutExisting);
 
       log.info("공지사항 배치 저장 완료 {}", noticesWithoutExisting.size());
     } catch (Exception e) {
@@ -46,7 +45,7 @@ public class AlrimEventBatch {
       val patchNotes = crawler.crawlPatchNotes();
       val patchNotesWithoutExisting = mapleLandAlrimBatchService.excludeExistingFromCrawledAlrims(AlrimType.PATCH_NOTE, patchNotes);
       commandBatchMediator.saveAll(patchNotesWithoutExisting);
-//      fcmCandidates.addAll(patchNotesWithoutExisting);
+      fcmCandidates.addAll(patchNotesWithoutExisting);
 
       log.info("패치노트 배치 저장 완료 {}", patchNotesWithoutExisting.size());
     } catch (Exception e) {
@@ -58,7 +57,7 @@ public class AlrimEventBatch {
       val events = crawler.crawlEvents();
       val eventsWithoutExisting = mapleLandAlrimBatchService.excludeExistingFromCrawledAlrims(AlrimType.EVENT, events);
       commandBatchMediator.saveAll(eventsWithoutExisting);
-//      fcmCandidates.addAll(eventsWithoutExisting.stream().filter(it -> !it.getOutdated()).toList());
+      fcmCandidates.addAll(eventsWithoutExisting.stream().filter(it -> !it.getOutdated()).toList());
 
       // 종료 상태 반영 및 누락된 진행중 이벤트 종료 처리
       mapleLandAlrimBatchService.syncEventOutdatedStatusFromCrawl(events);
